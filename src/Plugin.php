6f6d3d2be50054efebaa2b0098d463279910ee4e
Tooeo <oleg@tabota.ru>
2023-07-21 17:28:31 +0300

    fix composer scripts after install

--- src/Plugin.php
+++ src/Plugin.php
@@ -7,12 +7,15 @@ use Psalm\Plugin\PluginEntryPointInterface;
 use Psalm\Plugin\RegistrationInterface;
 use Tooeo\PsalmPluginJms\Hooks\JmsAnnotationCheckerHook;
 
+/**
+ * @psalm-suppress UnusedClass
+ */
 class Plugin implements PluginEntryPointInterface
 {
     /** @return void */
     public function __invoke(RegistrationInterface $psalm, ?SimpleXMLElement $config = null): void
     {
-        require_once __DIR__.'/Hooks/JmsAnnotationCheckerHook.php';
+        require_once __DIR__ . '/Hooks/JmsAnnotationCheckerHook.php';
         $psalm->registerHooksFromClass(JmsAnnotationCheckerHook::class);
     }
 }
